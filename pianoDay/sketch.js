f736d2316230a0153e985ef27a26fa0170002479
giy <giy.hands@gmail.com>
2019-03-13 16:21:23 +0900

    testing

--- pianoDay/sketch.js
+++ pianoDay/sketch.js
@@ -8,16 +8,23 @@ let baseYSeoul = 0
 let velLondon = 3
 let velSeoul = 1
 let wc = null
+let wc2 = null
+let DIRECTION = Object.freeze({"UP":1, "DOWN":2})
 
 class WaveCircle {
     constructor(_x, _y, _r) {
         this.x = _x
         this.y = _y
         this.r = _r
+        this.ampDirectionFlag = DIRECTION.UP 
     }
 
     update(bufferArray) {
         this.buffer = bufferArray
+    }
+
+    direction(_ampDirectionFlag) {
+        this.ampDirectionFlag = _ampDirectionFlag
     }
 
     draw() {
@@ -25,7 +32,7 @@ class WaveCircle {
 
         // noFill()
         let newValue = this.buffer[this.buffer.length - 1]
-        fill(10, 80, 200, (1 - newValue) * 1000)
+        fill(10, 80, 200)
         strokeWeight(0.4)
         stroke(0, 150)
 
@@ -35,10 +42,13 @@ class WaveCircle {
         let d = 360 / this.buffer.length
         for (var i = 0; i <this.buffer.length; i++) {
 
-            let x = this.r * cos(i * d)
-            let y = this.r * sin(i * d)
-            vertex(x, y - this.buffer[this.buffer.length - i] * 1000)
-            // vertex(w - (i*velLondon), baseYLondon + bufLondon[bufLondon.length - i] * 1000)
+            let x = this.r * cos(i * 1)
+            let y = this.r * sin(i * 1)
+            if (this.ampDirectionFlag === DIRECTION.UP) {
+                vertex(x, y - this.buffer[this.buffer.length - i] * 1000)
+            } else if (this.ampDirectionFlag === DIRECTION.DOWN) {
+                vertex(x, y + this.buffer[this.buffer.length - i] * 1000)
+            }
 
         }
         endShape()
@@ -56,15 +66,19 @@ function setup() {
     createCanvas(1280, 900)
     background(255)
 
-    wc = new WaveCircle(width/2, height/2, 200)
+    // Enum for direction flat
+
+    wc = new WaveCircle(width*1/4, height/2, 200)
+    wc2 = new WaveCircle(width*3/4, height/2, 100)
 
     // Audio In
     mic = new p5.AudioIn()
     mic.start();
 
     // Buffer Init
-    bufNumLondon = (width - (pad * 2))/velLondon
+    // bufNumLondon = (width - (pad * 2))/velLondon
     // bufNumSeoul = (width - (pad * 2))/velSeoul
+    bufNumLondon = 360 * 4
     bufNumSeoul = 360 * 4
     bufLondon.fill(0)
     bufSeoul.fill(0)
@@ -111,7 +125,12 @@ function draw() {
     }
 
     wc.update(bufSeoul)
+    wc.direction(DIRECTION.UP)
     wc.draw()
+
+    wc2.update(bufLondon)
+    wc2.direction(DIRECTION.DOWN)
+    wc2.draw()
 
 
 
